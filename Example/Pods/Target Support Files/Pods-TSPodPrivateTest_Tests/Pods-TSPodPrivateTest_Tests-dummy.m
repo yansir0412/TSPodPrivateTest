84a1f4a6f59fe88f3324952d5bc887345f9ba099
yanxin0905 <345387567@qq.com>
2021-10-16 16:17:07 +0800

    no message

---
+++ Example/Pods/Target Support Files/Pods-TSPodPrivateTest_Tests/Pods-TSPodPrivateTest_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TSPodPrivateTest_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TSPodPrivateTest_Tests
+@end
---
+++ Example/TSPodPrivateTest/YXViewController.h
@@ -0,0 +1,13 @@
+//
+//  YXViewController.h
+//  TSPodPrivateTest
+//
+//  Created by yanxin on 10/16/2021.
+//  Copyright (c) 2021 yanxin. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YXViewController : UIViewController
+
+@end
